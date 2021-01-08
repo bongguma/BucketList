2826580f8b7486789a563c6d10d873db35fc2554
kimyejin <7602kyj@e-mirim.hs.kr>
2021-01-08 22:58:46 +0900

    viewController 화면전환 작업

---
+++ BucketList/RegistBucketList.swift
@@ -0,0 +1,17 @@
+//
+//  RegistBucketList.swift
+//  BucketList
+//
+//  Created by 김예진 on 2021/01/08.
+//
+
+import UIKit
+
+class RegistBucketList : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("RegistBucketList!!!!!!!!!!!!")
+    }
+}
--- BucketList/ViewController.swift
+++ BucketList/ViewController.swift
@@ -12,8 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
     }
 
-
+    @IBAction func LoginAction(_ sender: Any) {
+        let registBuketList = self.storyboard?.instantiateViewController(withIdentifier: "RegistBucketList") as? RegistBucketList
+        
+        self.present(registBuketList!, animated: true)
+    }
+    
 }
 
