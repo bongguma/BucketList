ac75db5cc8a92c05cf670ce6fbb216c6298363a8
kimyejin <7602kyj@e-mirim.hs.kr>
2021-01-10 23:56:22 +0900

    버킷리스트 테이블 리스트 작업 시작 ( 디바이스 삭제 전까지 데이터 저장 로직 추가 예정 )

--- BucketList/RegistBucketList.swift
+++ BucketList/RegistBucketList.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class RegistBucketList : UIViewController {
+    
+    // 입력한 버킷리스트가 테이블형식으로 리스트화 됨
+    @IBOutlet weak var bucketListTableView: UITableView!
+    
+    // 현재 방금 입력한 버킷리스트 출력
+    @IBOutlet weak var nowBucketLabel: UILabel!
+    
+    // 자신의 버킷리스트를 입력하는 칸
+    @IBOutlet weak var insertBucketTxtF: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print("RegistBucketList!!!!!!!!!!!!")
+        
     }
 }
